af8235dfe8501d95e7bdd431de160d6d76c8465e
davidakoontz <david@koontz.name>
2023-11-16 16:19:09 -0700

    Initial Commit

---
+++ ExampleCalculatorDisplay/ExampleCalculatorDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleCalculatorDisplayApp.swift
+//  ExampleCalculatorDisplay
+//
+//  Created by David on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleCalculatorDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
